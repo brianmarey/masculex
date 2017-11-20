bbc0da75270ac5b66ba4edb56b347c09983ed82d
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-11-20 14:05:56 -0500

    Reconfiguring for multiple sites

--- src/main/java/com/careydevelopment/masculex/controller/BlogController.java
+++ src/main/java/com/careydevelopment/masculex/controller/BlogController.java
@@ -2,15 +2,16 @@ package com.careydevelopment.masculex.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BlogController {
 
-    @RequestMapping(value = "/blog", method=RequestMethod.GET)
-    public String youtubeDemo(Model model) {
-        return "blog";
+    @RequestMapping(value = "/{context}/blog", method=RequestMethod.GET)
+    public String youtubeDemo(@PathVariable String context, Model model) {
+        return context + "/blog";
     }
 	
 }
